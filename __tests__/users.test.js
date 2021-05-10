9f8ed2cef60e3f20099309dbf4d9b6a59691a413
Swapnil Agrawal <swapnilagarwal2001@gmail.com>
2021-05-10 15:37:26 +0530

    Removed serviceJson File

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,76 +1,69 @@
-const User = require('../src/controllers/users')
-const {
-    v4: uuid
-} = require('uuid')
+const Users = require("../src/controllers/users.js");
+const {v4:uuid} = require("uuid");
 
-test('User already added check', () => {
-    return User.add({
-        body: {
-            email: "shobhit@dutch.com"
-        }
-    }).then(data => {
-        expect(data.statusCode).toBe(403)
-        expect(data.error).toBe("Email Id already exists")
-    });
-})
+test('User Already Added Check',()=>{
+    return Users
+            .addUser({body:{email:"shobhit@dutch.com"}})
+            .then(data=>{
+                expect(data.statusCode).toBe(403)
+                expect(data.error).toBe("Email Id already exists")
+            });
+});
 
-describe("New User Added and verify otp", () => {
+describe("New User Added and verify otp",()=>{
     let user;
-    test("New User added", () => {
+    test("New User added",()=>{
         return Users.addUser({
-            body: {
-                email: uuid() + "@dutch.com"
-            }
-        }).then(data => {
-            user = {
-                id: data.id,
-                email: data.email,
-                otp: data.otp
-            }
-            expect(data.statusCode).toBe(200)
-        });
+                        body: {
+                            email:  uuid() + "@dutch.com"
+                        }
+                    }).then(data => {
+                        user = {
+                            id:data.id,
+                            email:data.email,
+                            otp:data.otp
+                        }
+                        expect(data.statusCode).toBe(200) 
+                    });
     });
-
-    test("Verify User Otp", () => {
+    
+    test("Verify User Otp",()=>{
         return Users.verifyUser({
-            body: {
-                id: user.id,
-                otp: user.otp,
-                email: user.email,
-            }
-        }).then(data => {
-            expect(data.statusCode).toBe(200)
-            expect(data.email).toBe(user.email)
-            expect(data.id).toBe(user.id)
-        })
+                    body:{
+                        id:user.id,
+                        otp:user.otp,
+                        email:user.email,
+                    }
+                }).then(data=>{
+                    expect(data.statusCode).toBe(200)
+                    expect(data.email).toBe(user.email) 
+                    expect(data.id).toBe(user.id) 
+                })
     })
-})
-
-test('New user is getting added', () => {
-    return User.add({
-        body: {
-            email: uuid() + "@dutch.com"
-        }
-    }).then(data => {
-        expect(data.statusCode).toBe(200)
-    });
 })
 
 test('Check friends getting added to the user', async () => {
 
-    let u1 = await User.add({
+    let u1 = await Users.addUser({
         body: {
             email: "shobhit@dutch.com"
         }
     })
 
-    let u2 = await User.add({
+    let u2 = await Users.addUser({
         body: {
-            email: uuid() + "@dutch.com"
+            email:  uuid() + "@dutch.com"
         }
     })
 
-    return User.addFriend({
+    u2 = await Users.verifyUser({
+        body:{
+            id:u2.id,
+            otp:u2.otp,
+            email:u2.email,
+        }
+    })
+    return Users.addFriend({
         body: {
             userId: u1.id,
             otherUser: u2.id
@@ -81,19 +74,19 @@ test('Check friends getting added to the user', async () => {
 })
 
 test('find all friends', async () => {
-    let u1 = await User.add({
+    let u1 = await Users.addUser({
         body: {
             email: "shobhit@dutch.com"
         }
     })
 
-    return User.fetchFriends({
-            userId: u1.id
-        })
-        .then(data => {
-            let ch = Array.isArray(data.data)
-            expect(data.statusCode).toBe(200)
-            // console.log(data.data)
-            expect(ch).toBe(true)
-        })
+    return Users.fetchFriends({
+        userId: u1.id
+    })
+    .then(data => {
+        let ch = Array.isArray(data.data)
+        expect(data.statusCode).toBe(200)
+        // console.log(data.data)
+        expect(ch).toBe(true)
+    })
 })
