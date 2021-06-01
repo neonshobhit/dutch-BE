033521877ec7152cf5220bcc16905fc383ad3279
Shobhit Maheshwari <shobhitmaheshwari18@gmail.com>
2021-06-02 00:56:45 +0530

    fix: function routes

--- functions/src/app.js
+++ functions/src/app.js
@@ -12,20 +12,20 @@ app.use(express.json());
 // app.use(morgan('tiny'))
 // Assigning multipe times so that we can depl
 // each one as an individual function at Cloud Funcitions
-// const users = app;
-// const records = app;
-// const friends = app;
-// const events = app;
+const users = app;
+const records = app;
+const friends = app;
+const events = app;
 
-// users.use("/users", require("./routers/users"));
-// records.use("/records", require("./routers/records"));
-// friends.use("/friends", require("./routers/friends"));
-// events.use("/events", require("./routers/event"));
+users.use("/users", require("./routers/users"));
+records.use("/records", require("./routers/records"));
+friends.use("/friends", require("./routers/friends"));
+events.use("/events", require("./routers/event"));
 
-app.use("/users", require("./routers/users"));
-app.use("/records", require("./routers/records"));
-app.use("/friends", require("./routers/friends"));
-app.use("/events", require("./routers/event"));
+// app.use("/users", require("./routers/users"));
+// app.use("/records", require("./routers/records"));
+// app.use("/friends", require("./routers/friends"));
+// app.use("/events", require("./routers/event"));
 
 // app.get('*', (req, res) => {
 //     res.send("hello world")
@@ -44,7 +44,9 @@ app.use("/events", require("./routers/event"));
 //     console.log("server is up and running")
 // })
 
-exports.module = functions.https.onRequest(app);
-// exports.records = functions.https.onRequest(records);
-// exports.friends = functions.https.onRequest(friends);
-// exports.events = functions.https.onRequest(events);
+// exports.module = functions.https.onRequest(app);
+
+exports.users = functions.https.onRequest(users);
+exports.records = functions.https.onRequest(records);
+exports.friends = functions.https.onRequest(friends);
+exports.events = functions.https.onRequest(events);
